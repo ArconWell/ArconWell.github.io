0a5a23b1163289696c44d03b145679e761fcab25
Alexander Vanyukov <arcon595@gmail.com>
2020-12-14 12:18:27 +0300

    Remove previous commit

--- scripts/extra-small-devides-behavior.js
+++ scripts/extra-small-devides-behavior.js
@@ -1,7 +1,7 @@
 var capture = document.getElementById('new-album-capture');
 
-if (window.matchMedia("(max-device-width: 330px)").matches){
-    capture.classList.add('display-4');
+if (window.matchMedia("(max-device-width: 320px)").matches){
+    capture.classList.remove('display-3');
 } else {
-    capture.classList.remove('display-4');
+    capture.classList.add('display-3');
 }
